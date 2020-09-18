2ce97973e6051e859bc6ae07c9293375a1b5d9c4
Frendy Guo <frendyguo@gmail.com>
2020-09-18 09:16:32 +0700

    fix: remove unused comment

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,14 +29,6 @@ const Layout = ({
   return (
     <div className="mLayout">
       <Header siteTitle={data.site.siteMetadata.title} />
-      {/* <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-      </div> */}
       <main className="mLayout-main">{children}</main>
       {
         withFooter && (
